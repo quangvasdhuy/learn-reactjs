3e8eee56d234fb9cdd3799e8838ed6e15330f8f2
huyle3 <lehuy3@hitachivantara.com>
2023-03-28 13:05:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-// import './App.css';
 import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
+import { Link, NavLink, Route } from 'react-router-dom';
 
 
 function App() {
   
   return (
     <div className="App">
-   <TodoFeature></TodoFeature>
+      Header
+
+      <p><Link to="/todos">TODO</Link></p>
+      <p><Link to="/albums">ALBUM</Link></p>
+
+      <p><NavLink to="/todos" activeClassName="active-name">TODO</NavLink></p>
+      <p><NavLink to="/albums">ALBUM</NavLink></p>
+
+      <Route path="/todos" component={TodoFeature}></Route>
+      <Route path="/albums" component={AlbumFeature}></Route>
+      Footer
     </div>
   );
 }
